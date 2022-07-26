616a6426ecc7a310b8a5319787a50f1c75eff7d7
Frank Feng <frankfeng1998@gmail.com>
2022-07-26 15:49:37 -0500

    Finish the sign-up form

---
+++ script.js
@@ -0,0 +1,12 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', e => {
+  const pass1 = document.querySelector('#password1');
+  const pass2 = document.querySelector('#password2');
+  const errorMsg = document.querySelector('.error-msg');
+  if (pass1 !== pass2) {
+    e.preventDefault();
+    pass1.classList.add('error');
+    pass2.classList.add('error');
+    errorMsg.style.display = 'block';
+  }
+});
